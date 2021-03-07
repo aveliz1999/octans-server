2951f1014d4cf72b60a84e0d00faa1bacd431464
aveliz1999 <github@veliz99.com>
2021-03-07 00:19:37 -0500

    Change media download path

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import {files} from "../../config";
 import multer from 'multer';
-import {uploadMedia, list, download} from "../controllers/media";
+import {uploadMedia, list, download, listTags} from "../controllers/media";
 
 const upload = multer({dest: `${files.fileDirectory}/in/`});
 
@@ -10,5 +10,5 @@ const router = express.Router();
 
 router.post('/upload', upload.single('file'), uploadMedia);
 router.post('/search', list);
-router.get('/:hash/:thumbnail?.(mp4|png)', download)
+router.get('/download/:hash/:thumbnail?.(mp4|png)', download)
 export default router;
